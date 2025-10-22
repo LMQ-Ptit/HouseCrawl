40e5d79b360fc1be75199f428e2aa50b436d9d07
Lương Minh Quý <luongminhquy360@gmail.com>
2025-10-22 21:43:41 +0700

    k lệch cột

--- main.py
+++ main.py
@@ -263,7 +263,7 @@ if __name__ == "__main__":
     # Đọc các URL từ file CSV
     print("📂 Đang đọc danh sách URL...")
     urls = read_urls_from_csv(input_csv)
-    urls=urls[20511:]
+    urls=urls[3241:]
     
     if urls:
         print(f"🔍 Đã tìm thấy {len(urls)} URL để xử lý với {num_processes} tiến trình\n")
